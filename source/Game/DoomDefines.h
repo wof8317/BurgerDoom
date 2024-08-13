ed8ce24a307b43a7bf1e52afadd23543df873002
atsb <44937323+atsb@users.noreply.github.com>
2024-08-13 19:56:05 +0200

    Put back some settings from vanilla
    also improve integer / tick scaling speed timings

--- source/Game/DoomDefines.h
+++ source/Game/DoomDefines.h
@@ -23,11 +23,11 @@ static constexpr float      SECS_PER_TICK   = 1.0f / (float) TICKSPERSEC;   // T
 static constexpr uint32_t   MAPBLOCKSHIFT   = FRACBITS + 7;                 // Shift value to convert Fixed to 128 pixel blocks
 static constexpr Fixed      ONFLOORZ        = FRACMIN;                      // Attach object to floor with this z
 static constexpr Fixed      ONCEILINGZ      = FRACMAX;                      // Attach object to ceiling with this z
-static constexpr Fixed      GRAVITY         = (FRACUNIT * 35) / 60;         // Rate of fall (DC: bugfix - convert to 3DO timebase properly. Fall speed in 3DO version was too much originally!)
+static constexpr Fixed      GRAVITY         = 4 * FRACUNIT;                 // Rate of fall
 static constexpr Fixed      MAXMOVE         = 16 * FRACUNIT;                // Maximum velocity
-static constexpr Fixed      MAXRADIUS       = 32 * FRACUNIT;                // Largest radius of any critter
-static constexpr Fixed      MELEERANGE      = 70 * FRACUNIT;                // Range of hand to hand combat
-static constexpr Fixed      MISSILERANGE    = 32 * 64 * FRACUNIT;           // Range of guns targeting
+static constexpr Fixed      MAXRADIUS       = 32 << FRACBITS;                // Largest radius of any critter
+static constexpr Fixed      MELEERANGE      = 70 << FRACBITS;                // Range of hand to hand combat
+static constexpr Fixed      MISSILERANGE    = 32 * 64 << FRACBITS;           // Range of guns targeting
 static constexpr Fixed      FLOATSPEED      = 8 * FRACUNIT;                 // Speed an object can float vertically
 static constexpr Fixed      SKULLSPEED      = 40 * FRACUNIT;                // Speed of the skull to attack
 
@@ -56,10 +56,9 @@ enum {
 // Tries to round so the answer is as close as possible.
 //------------------------------------------------------------------------------------------------------------------------------------------
 inline constexpr uint32_t convertPcTicks(const uint32_t ticks35Hz) noexcept {
-    // Get the tick count in 31.1 fixed point format by multiplying by TICKSPERSEC/35 (in 31.1 format).
-    // When returning the integer answer round up if the fractional part is '.5':
-    const uint32_t tickCountFixed = ((ticks35Hz * uint32_t(TICKSPERSEC)) << 2) / (uint32_t(35) << 1);
-    return (tickCountFixed & 1) ? (tickCountFixed >> 1) + 1 : (tickCountFixed >> 1);
+    constexpr uint32_t scaleFactor = (TICKSPERSEC << 1) / 35;
+    const uint32_t tickCountFixed = ticks35Hz * scaleFactor;
+    return (tickCountFixed + 1) >> 1;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -67,8 +66,7 @@ inline constexpr uint32_t convertPcTicks(const uint32_t ticks35Hz) noexcept {
 // Tries to round so the answer is as close as possible.
 //------------------------------------------------------------------------------------------------------------------------------------------
 inline constexpr uint32_t convertPcUintSpeed(const uint32_t speed35Hz) noexcept {
-    // Get the tick count in 31.1 fixed point format by multiplying by 35/TICKSPERSEC (in 31.1 format).
-    // When returning the integer answer round up if the fractional part is '.5':
-    const uint32_t speedFixed = ((speed35Hz * uint32_t(35)) << 2) / (uint32_t(TICKSPERSEC) << 1);
-    return (speedFixed & 1) ? (speedFixed >> 1) + 1 : (speedFixed >> 1);
+    constexpr uint32_t scaleFactor = (35 << 1) / TICKSPERSEC;
+    const uint32_t speedFixed = speed35Hz * scaleFactor;
+    return (speedFixed + 1) >> 1;
 }
